656c8cedc807fba2c0c0d00d4acf6adf62894c89
utkarshgour1 <utkarshgour37@gmail.com>
2024-11-27 00:01:00 +0530

    Menu Script added

---
+++ menu_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo Press any option
+echo 1=Show Date
+echo 2=List files in current directory
+echo 3=Show username 
+
+read choice 
+
+case $choice in 
+	1)date;;
+	2)ls -ltr;;
+	3)whoami;;
+	*) echo Invalid input 
+esac
