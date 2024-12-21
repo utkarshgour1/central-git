91a83df4870eae3995dac48d300a54f0194f3b81
Utkarsh Gour <utkarshgour37@gmail.com>
2024-12-21 22:03:25 +0530

    new update menu_script.sh

--- menu_script.sh
+++ menu_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ech Press the any option
+echo Press the any option
 echo 1=Show me the Date
 echo 2=List files in current directory
 echo 3=Show username 
@@ -8,7 +8,7 @@ echo 3=Show username
 read choice 
 
 case $choice in 
-	1)date;;
+	1)date;)
 	2)ls -ltr;;
 	3)whoami;;
 	*) echo Invalid input 
